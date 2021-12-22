d7b4207a46131a2680a430ff39bc6ffab017710e
Shiina-Yukari <57476839+Shiina-Yukari@users.noreply.github.com>
2021-12-22 18:43:09 +0800

    凭感觉写的第一版

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # remote_control
-基于HoshinoBot的bot私聊消息/好友申请等信息定向转发
+基于[HoshinoBot](https://github.com/Ice-Cirno/HoshinoBot )的bot私聊消息/好友申请等信息定向转发
+
+## 当前功能
+* 审批入群邀请 
+    * 分全部拒绝、全部同意、由bot管理员审批、仅同意特定QQ号邀请四个模式
+    * 需要先将bot的QQ设置为审批入群
+    * 现在被邀请进50人以下的群默认是不需要同意的，这部分的处理方法之后再补
+* 审批好友申请
+    * 分全部拒绝、全部同意、由bot管理员审批、仅同意以特定理由发起的申请四个模式
+    * 同上，需要先将bot的QQ设置为审批加好友
+* 私聊消息中继
+    * 将bot收到的所有私聊消息转发给指定QQ号
+    * 转发对象需与bot的QQ互为好友
+* 黑名单
+    * bot将不会加入列入黑名单中的群，不会同意来自列入黑名单中的QQ的入群邀请/好友申请，不会转发来自列入黑名单中的QQ的私聊消息
+    * 黑名单的优先级高于以上功能的模式
+
+## 使用方法
+* 在hoshinobot的`modules`文件夹内：
+    > git clone https://github.com/Shiina-Yukari/remote_control.git
+
+* 按说明修改`modules/remote_control/config.json`配置文件内容。
+
+* 在`config`文件夹的`__bot__.py`内加入`remote_control`模组：
+    > MODULES_ON = {
+    > 
+    > ...
+    > 
+    >     'remote_control',
+    > 
+    > ...
+    > 
+    > }
---
+++ __init__.py
@@ -0,0 +1,144 @@
+import json
+import os
+import time
+
+import nonebot
+from aiocqhttp.default import on_message
+from nonebot import RequestSession, on_request, on_command, CommandSession
+
+#import hoshino
+from hoshino import Service, logger
+from hoshino.typing import CQEvent
+
+import suid
+
+sv = Service('remote_control', visible= True, enable_on_default= True, bundle='remote_control', help_='''
+控制你的bot
+'''.strip())
+config_default = {
+    "group_invite": {
+        "mode": 0, # 0：全部拒绝；1：全部同意；2：管理员审批；3：仅同意来自特定QQ号的邀请
+        "monitor": "123", # 模式2里的管理员QQ
+        "admin": ["123", "234","345"] # 模式3里的特定QQ号列表
+    },
+    "friend_invite": {
+        "mode": 0, # 0：全部拒绝；1：全部同意；2：管理员审批；3：仅同意以特定理由发起的申请
+        "monitor": "123", # 模式2里的管理员QQ
+        "keyword": "miracle_word" # 模式3里的特定理由内容
+    },
+    "relay": {
+        "mode": 0, # 0：关闭；1：打开
+        "monitor": "123123123" # 消息转发目标QQ
+    },
+    "blacklist": {
+        "group": ["123", "234","345"], # 群组黑名单
+        "friend": ["123", "234","345"] # 用户黑名单
+    },
+    "reason": {
+        "reject": "请联系bot管理组成员审批。", # 审批被拒绝（或超时）时的理由文本
+        "blacklist": "DO NOT DISTURB." # 拒绝黑名单申请时的理由文本
+    }
+}
+pathcfg = os.path.join(os.path.dirname(__file__), 'config.json')
+if not os.path.exists(pathcfg):
+    try:
+        with open(pathcfg, 'w') as cfgf:
+            json.dump(config_default, cfgf, ensure_ascii=False, indent=2)
+            logger.error('[WARNING]未找到配置文件，已根据默认配置模板创建，请打开插件目录内config.json查看和修改。')
+    except:
+        logger.error('[ERROR]创建配置文件失败，请检查插件目录的读写权限及是否存在config.json。')
+config = json.load(open(pathcfg, 'r', encoding='utf8'))
+approve_dict = dict()
+
+def get_config(key, sub_key):
+    if key in config and sub_key in config[key]:
+        return config[key][sub_key]
+    return None
+
+
+@on_request('group.invite')
+async def handle_group_invite(session: RequestSession):
+    mode = get_config("group_invite", "mode")
+    uid = str(session.ctx['user_id'])
+    gid = str(session.ctx['group_id'])
+    if gid not in get_config("blacklist", "group") and uid not in get_config("blacklist", "friend"):
+        if mode == 1:
+            await session.approve()
+        elif mode == 2:
+            uuid = suid.short_uuid()
+            approve_dict[uuid] = False
+            monitor_message = f"收到来自QQ：{uid} 的邀请入群通知，群号：{gid}\n若同意，请回复 botctrl {uuid}\n此消息发送时间为{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}，有效时长30分钟。"
+            await session.bot.send_private_msg(user_id=int(get_config("group_invite", "monitor")),
+                                               message=monitor_message)
+            for i in range(0,6):
+                time.sleep(300)
+                if approve_dict[uuid]:
+                    await session.approve()
+                    break
+            if not approve_dict[uuid]:
+                await session.reject(reason=get_config("reason", "reject"))
+            approve_dict.pop(uuid, "nokey")
+        elif mode == 3:
+            if uid in get_config("group_invite", "admin"):
+                await session.approve()
+            else:
+                await session.reject(reason=get_config("reason", "reject"))
+    else:
+        await session.reject(reason=get_config("reason", "blacklist"))
+
+
+@on_request('friend')
+async def handle_friend_invite(session: RequestSession):
+    mode = get_config("friend_invite", "mode")
+    uid = str(session.ctx['user_id'])
+    if uid not in get_config("blacklist", "friend"):
+        if mode == 1:
+            await session.approve()
+        elif mode == 2:
+            uuid = suid.short_uuid()
+            approve_dict[uuid] = False
+            monitor_message = f"收到来自QQ：{uid} 的好友申请。\n若同意，请回复 botctrl {uuid}\n此消息发送时间为{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}，有效时长30分钟。"
+            await session.bot.send_private_msg(user_id=int(get_config("group_invite", "monitor")),
+                                               message=monitor_message)
+            for i in range(0,6):
+                time.sleep(300)
+                if approve_dict[uuid]:
+                    await session.approve()
+                    break
+            if not approve_dict[uuid]:
+                await session.reject(reason=get_config("reason", "reject"))
+            approve_dict.pop(uuid, "nokey")
+        elif mode == 3:
+            comm = session.ctx['comment']
+            if comm == get_config("friend_invite", "keyword"):
+                await session.approve()
+            else:
+                await session.reject(reason=get_config("reason", "reject"))
+    else:
+        await session.reject(reason=get_config("reason", "blacklist"))
+
+
+@on_command('botctrl')
+async def bot_control(session: CommandSession):
+    suid = session.current_arg_text.strip()
+    if not suid:
+        suid = (await session.aget(prompt='请发送审批UID。')).strip()
+    if suid in approve_dict:
+        approve_dict[suid] = True
+        await session.send("已完成审批。")
+    else:
+        await session.send("无该审批UID。")
+
+
+@on_message('private')
+async def msg_relay(session: CommandSession):
+    mode = get_config("relay", "mode")
+    if mode == 1:
+        uid = str(session.ctx['user_id'])
+        if uid not in get_config("blacklist", "friend"):
+            monitor = int(get_config("group_invite", "monitor"))
+            type = session.ctx['sub_type']
+            msg = session.ctx['message']
+            relay_header = f"收到来自QQ：{uid} 的私聊消息，消息类型：{type}。\n以下为消息内容："
+            await session.bot.send_private_msg(user_id=monitor, message=relay_header)
+            await session.bot.send_private_msg(user_id=monitor, message=msg)
